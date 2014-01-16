8f2968dcfea5bc0632631252277351ba15ef6334
Darren Frenkel <darrenfrenkel@gmail.com>
2014-01-16 23:58:44 +0000

    added these files to the settings directory

---
+++ microblog/settings/local.py
@@ -0,0 +1,13 @@
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'microblog',
+        'USER': 'vagrant',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
