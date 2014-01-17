580474cbf1839f2a7213a5cda552d7e5f0848691
Darren Frenkel <darrenfrenkel@gmail.com>
2014-01-17 01:19:34 +0000

    updated how I configure the database on base.py

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -34,7 +34,9 @@ MANAGERS = ADMINS
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
